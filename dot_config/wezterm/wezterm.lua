0b137c8fb107f418e08751a9625bc480e78f4714
Michael Steinbaugh <mike@steinbaugh.com>
2025-01-04 15:43:03 -0500

    Set default font size

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,13 @@
+-- WezTerm configuration
+--
+-- @note Updated 2025-01-04.
+--
+-- @seealso
+-- * https://draculatheme.com/wezterm
+
 return {
     color_scheme = "Dracula (Official)",
+    font_size = 16,
     tab_bar_at_bottom = true,
     use_fancy_tab_bar = false,
     window_decorations = "RESIZE"

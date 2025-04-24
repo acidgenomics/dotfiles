a01e64ffdaae9ed76dc848bfc6e861070d842301
Michael Steinbaugh <mike@steinbaugh.com>
2025-04-24 12:19:14 -0400

    Simplify lsp config

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -1,42 +1,3 @@
---[[
-nvim-lspconfig
-
-* https://github.com/neovim/nvim-lspconfig
-
-Useful commands:
-:help lsp-quickstart
-:help lspconfig
-:help lspconfig-all
-:checkhealth lsp
-
-Can enable debugging with:
-> vim.lsp.set_log_level("debug")
-]]
-
---[[
-mason-lspconfig (alternate configuration)
-
-* https://github.com/williamboman/mason-lspconfig.nvim
-* https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-
-:help mason-lspconfig.nvim
-:help mason-lspconfig-introduction
-:help mason-lspconfig-quickstart
-:help mason-lspconfig-settings
-
-> require("mason").setup()
-> require("mason-lspconfig").setup()
-
-After setting up mason-lspconfig you may set up servers via lspconfig
-> require("lspconfig").lua_ls.setup {}
-> require("lspconfig").rust_analyzer.setup {}
-> ...
-
-> require("mason-lspconfig").setup {
->     ensure_installed = { "pyright", "r_language_server" },
-> }
-]]
-
 -- Bash
 vim.lsp.enable('bashls')
 
@@ -45,25 +6,5 @@ vim.lsp.enable('bashls')
 vim.lsp.enable('ruff')
 
 -- R
-vim.lsp.enable('r_language_server')
-
--- Air (for R)
--- https://posit-dev.github.io/air/editor-neovim.html
-require("lspconfig").air.setup({
-    on_attach = function(_, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format()
-            end,
-        })
-    end,
-})
-
-require("lspconfig").r_language_server.setup({
-    on_attach = function(client, _)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-    end,
-})
-
+-- > vim.lsp.enable('r_language_server')
+vim.lsp.enable('air')

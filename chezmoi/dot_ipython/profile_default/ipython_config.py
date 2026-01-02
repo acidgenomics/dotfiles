0ca69d37b31c83452e647f9ff2de2f653a157dc0
Michael Steinbaugh <mike@steinbaugh.com>
2026-01-02 14:48:44 -0500

    Update config

--- chezmoi/dot_ipython/profile_default/ipython_config.py
+++ chezmoi/dot_ipython/profile_default/ipython_config.py
@@ -1,12 +1,24 @@
-"""
-IPython default configuration.
-Updated 2022-05-27.
+"""IPython default configuration.
 
 Generate default template with 'ipython profile create default'.
+
+Theme configuration details:
+https://ipython.readthedocs.io/en/stable/config/details.html
+
+Dracula configuration:
+https://draculatheme.com/pygments
 """
+
+from copy import deepcopy
+from IPython.utils.PyColorize import linux_theme, theme_table
 
 # pylint: disable=undefined-variable
 c = get_config()  # noqa
 
-c.TerminalInteractiveShell.highlighting_style = "dracula"
+dracula = deepcopy(linux_theme)
+dracula.base = "dracula"
+theme_table["dracula"] = dracula
+
+c.TerminalInteractiveShell.colors = "dracula"
+c.TerminalInteractiveShell.editor = "nvim"
 c.TerminalInteractiveShell.true_color = True

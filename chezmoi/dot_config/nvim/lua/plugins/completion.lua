53db71a151e3842279a509e46dcdf9fcfccaa37f
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-11 09:46:55 -0400

    Rework nvim completion defaults

--- chezmoi/dot_config/nvim/lua/plugins/completion.lua
+++ chezmoi/dot_config/nvim/lua/plugins/completion.lua
@@ -4,7 +4,20 @@ return {
         event = 'InsertEnter',
         version = '*',
         opts = {
-            keymap = { preset = 'default' },
+            enabled = function()
+                local disabled = { markdown = true, org = true, text = true }
+                return not disabled[vim.bo.filetype]
+            end,
+            keymap = {
+                preset = 'none',
+                ['<Tab>'] = { 'show', 'select_next', 'fallback' },
+                ['<S-Tab>'] = { 'select_prev', 'fallback' },
+                ['<C-y>'] = { 'select_and_accept', 'fallback' },
+                ['<C-e>'] = { 'cancel', 'fallback' },
+                ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+                ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+                ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+            },
             appearance = {
                 nerd_font_variant = 'mono',
             },
@@ -12,6 +25,7 @@ return {
                 default = { 'lsp', 'path', 'buffer' },
             },
             completion = {
+                menu = { auto_show = false },
                 documentation = { auto_show = true },
             },
         },

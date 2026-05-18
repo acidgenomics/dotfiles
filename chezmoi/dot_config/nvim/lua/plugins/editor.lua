c49384846250251ff2cb00bdfcdf7275b281fb91
Michael Steinbaugh <mike@steinbaugh.com>
2026-05-18 08:52:58 -0400

    Update neovim config

--- chezmoi/dot_config/nvim/lua/plugins/editor.lua
+++ chezmoi/dot_config/nvim/lua/plugins/editor.lua
@@ -29,6 +29,34 @@ return {
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         opts = {},
     },
+    -- Sidebar file tree
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = 'v3.x',
+        cmd = 'Neotree',
+        keys = {
+            { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle file tree' },
+        },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'MunifTanjim/nui.nvim',
+        },
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+                follow_current_file = { enabled = true },
+            },
+            window = {
+                position = 'left',
+                width = 35,
+            },
+        },
+    },
     -- Surround (Lua rewrite of tpope/vim-surround)
     {
         'kylechui/nvim-surround',

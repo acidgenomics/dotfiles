110cdb81f38c08ac814bb72ef34008aa62195d72
Michael Steinbaugh <mike@steinbaugh.com>
2026-06-06 13:05:28 -0400

    Improve color support consistency

--- chezmoi/dot_config/spacemacs/user-config.el
+++ chezmoi/dot_config/spacemacs/user-config.el
@@ -34,3 +34,19 @@
 (add-to-list 'auto-mode-alist '("\\.ronn$" . poly-markdown-mode))
 
 (put 'lisp-indent-local-overrides 'safe-local-variable #'listp)
+
+;; Dracula Pro themes (when installed).
+(let ((dp-themes (expand-file-name "~/.config/doom/themes")))
+  (when (file-directory-p dp-themes)
+    (add-to-list 'custom-theme-load-path dp-themes)))
+
+;; Automatic dark/light mode switching (macOS only).
+(when (featurep 'ns)
+  (let* ((dp-themes (expand-file-name "~/.config/doom/themes"))
+         (has-dp (file-directory-p dp-themes))
+         (dark-theme  (if has-dp 'dracula-pro-pro 'dracula))
+         (light-theme (if has-dp 'dracula-pro-alucard 'spacemacs-light)))
+    (setq auto-dark-dark-theme dark-theme
+          auto-dark-light-theme light-theme
+          auto-dark-polling-interval-seconds 2)
+    (auto-dark-mode t)))
